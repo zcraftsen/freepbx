068ef8a1400530b5aa3bf58a7115ae1d1c9aaa6f
Bell Li <bell.li@tlscontact.com>
2020-07-30 09:30:03 +0800

    first commit

--- freepbx-install.sh
+++ freepbx-install.sh
@@ -81,8 +81,8 @@ mysql -u root <<EOF
 UPDATE mysql.user SET authentication_string=PASSWORD('') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS asterisk;
-DELETE FROM mysql.db WHERE Db='asterisk' OR Db='asterisk\\_%';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
 
