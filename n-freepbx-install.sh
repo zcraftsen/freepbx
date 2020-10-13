563d175ebdb9e72377c98d6832871cc7193bdab9
Bell Li <bell.li@tlscontact.com>
2020-10-13 18:59:48 +0800

    update script

--- n-freepbx-install.sh
+++ n-freepbx-install.sh
@@ -294,6 +294,7 @@ else
 echo -e "\n\033[5;4;47;34m Please do "yum update -y" before running the installation \033[0m\n"
 echo -e "\n\033[5;4;47;34m Running yum update \033[0m\n"
 yum clean all
+sleep 3
 yum update -y
 echo -e "\n\033[5;4;47;34m System Rebooting, Please wait...\033[0m\n"
 sleep 5
